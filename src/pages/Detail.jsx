247b446c179e495eaa473255cec63b0c016d4178
dxxdev <diyorbekxojamberdiyevn1@gmail.com>
2024-01-22 13:41:09 +0500

    Commentaries section added

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -24,13 +24,12 @@ const Detail = () => {
   const [render, setRender] = useState();
   const [clientName, setClientName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
-  const [activeTab, setActiveTab] = useState("description");
-  // console.log(points);
-  useEffect(() => {
-    window.scrollTo({
-      top: 0,
-    });
-  }, []);
+  const [activeTab, setActiveTab] = useState("commentaries");
+  // useEffect(() => {
+  //   window.scrollTo({
+  //     top: 0,
+  //   });
+  // }, []);
   return (
     <div className={`${styles.container}`}>
       <div className="pt-12 pb-6 flex flex-col md:flex-row gap-6 lg:gap-12">
@@ -462,7 +461,7 @@ const Detail = () => {
           </div>
         </div>
       </div>
-      <div className="pt-6 pb-32 flex flex-col items-start">
+      <div className="pt-6 pb-6 flex flex-col items-start">
         <Tabs value={activeTab} className="w-full">
           <TabsHeader
             className="rounded-none border-b-2 border-b-transparent w-max flex gap-6 bg-transparent p-0"
@@ -497,9 +496,57 @@ const Detail = () => {
             </TabPanel>
             <TabPanel
               value="commentaries"
-              className="p-0 py-6 text-base leading-[164%] text-jet-black font-normal"
+              className="p-0 py-6 flex flex-col gap-6 text-base leading-[164%] text-jet-black font-normal"
             >
-              commentaries
+              <div className="flex justify-between items-center">
+                <div className="flex gap-2 items-center">
+                  <h3 className="text-2xl font-semibold leading-[112%] text-jet-black">
+                    Barcha sharhlar
+                  </h3>
+                  <p className="text-base text-dark-gray font-normal leading-[164%]">
+                    ({info.commentaries.length})
+                  </p>
+                </div>
+                <div className="flex gap-2 items-stretch">
+                  <select
+                    className="text-base leading-[164%] font-normal text-jet-black px-5 py-3 rounded-full bg-soft-gray"
+                    name=""
+                    id=""
+                    defaultValue="top"
+                  >
+                    <option value="top">Top kommentlar</option>
+                  </select>
+                  <button className="py-3 px-5  bg-jet-black rounded-full text-white leading-[164%] text-base font-normal">
+                    Fikr qoldirish
+                  </button>
+                </div>
+              </div>
+              <ul className="grid grid-cols-1 md:grid-cols-2 gap-2.5">
+                {info.commentaries &&
+                  info.commentaries.map((comment) => {
+                    return (
+                      <div
+                        key={comment.id}
+                        className="flex flex-col gap-6 justify-between px-8 py-7 rounded-[20px] border border-soft-gray"
+                      >
+                        <div className="flex flex-col gap-4">
+                          <RatingBar product={comment} />
+                          <div className="flex flex-col gap-3">
+                            <h4 className="text-jet-black font-semibold text-xl leading-[112%]">
+                              {comment.from}
+                            </h4>
+                            <blockquote className="text-sm leading-[112%] font-normal text-dark-gray">
+                              {comment.comment}
+                            </blockquote>
+                          </div>
+                        </div>
+                        <p className="text-dark-gray text-base leading-[164%]">
+                          {comment.date}
+                        </p>
+                      </div>
+                    );
+                  })}
+              </ul>
             </TabPanel>
           </TabsBody>
         </Tabs>
