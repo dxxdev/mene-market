50cdc90db300e08bd8de56ba5b326d97dc088e4e
dxxdev <diyorbekxojamberdiyevn1@gmail.com>
2024-01-26 13:02:49 +0500

    Added scroll up when moving to pages

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const MainLayout = ({ render, setRender }) => {
   const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [pathname]);
   return (
     <div className="flex w-full min-h-screen flex-col justify-between">
       <Header rendered={render} />
